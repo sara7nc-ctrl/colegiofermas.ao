0ef0aa92d26a62c0f54b7ac3e2ecb53be88b662b
sara7nc-ctrl <sara7nc@gmail.com>
2026-09-24 09:13:09 +0100

    Add section header 'Sobre Nós' to sobre.js

--- secciones/sobre.js
+++ secciones/sobre.js
@@ -6,11 +6,12 @@ document.getElementById('app-sections').insertAdjacentHTML('beforeend', `
 <!-- ================= SOBRE NÓS ================= -->
   <section class="view" data-view="sobre">
     <div class="section-head">
-      <h1>Sobre Nós</h1>
+      
       
     </div>
     <div class="about-grid">
       <div class="about-text">
+      <h1>Sobre Nós</h1>
        <p>O Colégio Fermas é uma instituição tutelada pelo Ministério da Educação, reconhecida a nível ministerial e municipal.</p>
         <p>Iniciou a sua actividade lectiva no dia 6 de fevereiro de 2015, com o ensino pré-escolar, o ensino primário e o I ciclo. Está situado no Lar Patriota, comuna do Benfica, pertencente ao município do Talatona.</p>
         <p>Acreditamos que aprender é também crescer como pessoa. Por isso, unimos rigor académico e acompanhamento próximo, num ambiente onde professores, alunos e famílias trabalham lado a lado.</p>
